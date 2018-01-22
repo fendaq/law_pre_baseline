1e3768f1cc39c4c4a3b00ec2d270a9198243cc8a
xiaocj <xiaocj16@mails.tsinghua.edu.cn>
2018-01-22 21:05:06 +0800

    used extract the related law articles

--- classifier.py
+++ classifier.py
@@ -1,13 +1,19 @@
+#author: xcj
+#this program is used to train SVM classifier!
+
 import json
 import os
 from scipy.stats import chisquare
-from sklearn.feature_extraction.text import TfidfVectorizer as TFIDF
 from sklearn.externals import joblib
 from gensim import corpora
 from gensim.corpora.textcorpus import TextCorpus
 from gensim.corpora.mmcorpus import MmCorpus
 from gensim.models.tfidfmodel import TfidfModel
-
+from scipy.sparse import csr_matrix
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import chi2
+from sklearn.svm import LinearSVC
+from gensim import matutils
 
 
 inpath = '/disk/mysql/law_data/final_data/'
@@ -32,27 +38,60 @@ def init():
 init()
 
 
-
-
 class classifier():
-    def __init__(self, num, dim):
+    def __init__(self, num, dim = 2000):
         self.num = num   #罪名代号
-        self.accusationName = accusation_list[num] #罪名名称
+        #self.accusationName = accusation_list[num] #罪名名称
         self.dim = dim  #降维之后的向量维数
+        self.totalSize = 0
+        self.corpus = []
+        self.label = []
+        self.oneSize = 0
+        self.zeroSize = 0
+        self.model = LinearSVC()
 
-    def getData(self):
+    def addData(self, text, label):
+        global dictionary
+        global tfidf
+        if label == self.num:
+            self.totalSize += 1
+            if self.oneSize <= 300:
+                self.label.append(1)
+                self.oneSize += 1
+            else:
+                return
+        else:
+            self.zeroSize += 1
+            if self.zeroSize % 1000 == 0 and self.zeroSize <= 400:
+                self.label.append(0)
+            else:
+                return
+        self.corpus.append(tfidf[dictionary.doc2bow(text.split())])
 
-        pass
+    def dataToMatrix(self):
+        data = []
+        rows = []
+        cols = []
+        for i, line in enumerate(self.data):
+            for elem in line:
+                rows.append(i)
+                cols.append(elem[0])
+                data.append(elem[1])
+        self.matrix = csr_matrix((data,(rows,cols)))
+        #释放数据，减少内存使用？
+
 
     def train(self):
         self.getData()
+        self.dataToMatrix()
         self.chisquare()
-
+        self.model.fit(self.matrix, self.label)
+        joblib.dump(self.model, str(self.num) + '.model')
 
 
     def chisquare(self):
-        pass
+        self.matrix = SelectKBest(chi2, k = self.dim).fit_transform(self.matrix, self.label)
 
-    def predict(self, text):
-        pass
+
+
 
